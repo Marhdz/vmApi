600b2ea90b50ed068ab1396d26a6789d8d90f2fd
Marhdz <33272156+Marhdz@users.noreply.github.com>
2020-07-07 17:45:17 -0600

    Update app.js
    cors origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors({origin: 'https://api-vm.herokuapp.com'}));
+app.use(cors({origin: 'http://localhost:3000'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
