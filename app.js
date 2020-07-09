d3a76ff2be23a6209f6097a2ecfd280b0f7371df
Marhdz <33272156+Marhdz@users.noreply.github.com>
2020-07-08 19:55:11 -0600

    cors origin heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'https://voicemailapp.herokuapp.com'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
